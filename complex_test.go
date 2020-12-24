2393f9f4bafd3d5422eb394b05f3aca32fb05134
Andrew Snodgrass <git@pointlander.info>
2020-12-23 19:26:45 -0700

    Update names

--- complex_test.go
+++ complex_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 The Big Authors. All rights reserved.
+// Copyright 2020 The C0mpl3x Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
